d65e76a35b454d08115a8a4419de9039e9b1245f
romadijen <168550890+romadijen@users.noreply.github.com>
2025-04-05 05:28:30 +1000

    Add template

---
+++ lesson-103/main.js
@@ -0,0 +1,56 @@
+// 1
+
+// const user = {
+//     name: 'Roman',
+//     age: 20,
+//     hairColor: 'brown',
+//     height: 180,
+//     eyeColor: 'blue',
+//     sayHello(name) {
+//         console.log(`Hello ${name}`)
+//     }
+// }
+
+// console.log(user)
+// user.sayHello('Alex')
+
+// ============================================================
+
+// 3
+
+const users = [
+    {
+        name: 'Alex',
+        age: 20,
+        isAdmin: true
+    },
+    {
+        name: 'Jake',
+        age: 23,
+        isAdmin: false
+    },
+    {
+        name: 'Tom',
+        age: 21,
+        isAdmin: true
+    },
+    {
+        name: 'Max',
+        age: 24,
+        isAdmin: false
+    }
+]
+
+let regularUsersCount = 0
+
+for (let i = 0; i<users.length; i++) {
+    if (users[i].isAdmin === false) {
+        regularUsersCount++;
+    }
+}
+
+console.log(regularUsersCount)
+
+
+
+
